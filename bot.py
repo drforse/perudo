5172d5794f5b3e06bc9f28f4c75d71391e35006c
dr_forse <george.lifeslice@gmail.com>
2020-01-29 18:56:29 +0000

    stop adding private chats to groups collection

--- bot.py
+++ bot.py
@@ -15,17 +15,20 @@ developers = [500238135]
 
 
 async def check_group_and_user(chat_id, user_id):
-    group = groups_col.find_one({'group_id': chat_id})
-    if not group:
-        groups_col.insert_one({'group_id': chat_id,
-                               'users': [user_id]})
-        group = groups_col.find_one({'group_id': chat_id})
     user = users_col.find_one({'user_id': user_id})
     if not user:
         users_col.insert_one({'user_id': user_id,
                               'years': 100,
                               'games_finished': 0,
                               'loses': 0})
+    grp = await bot.get_chat(chat_id)
+    if grp.stats == 'private':
+        return
+    group = groups_col.find_one({'group_id': chat_id})
+    if not group:
+        groups_col.insert_one({'group_id': chat_id,
+                               'users': [user_id]})
+        group = groups_col.find_one({'group_id': chat_id})
     if user_id not in group['users']:
         groups_col.update_one({'group_id': chat_id},
                               {'$push': {'users': user_id}})
